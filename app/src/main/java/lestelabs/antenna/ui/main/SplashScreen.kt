f8876a748c3d16f540e7d7a19f20404b22153ea1
lestelor <claudi.fauli@gmail.com>
2020-08-11 14:38:58 +0200

    v1.0

--- app/src/main/java/lestelabs/antenna/ui/main/SplashScreen.kt
+++ app/src/main/java/lestelabs/antenna/ui/main/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import lestelabs.antenna.R
 
 
 class SplashScreen : AppCompatActivity() {
@@ -15,7 +16,6 @@ class SplashScreen : AppCompatActivity() {
     private val DURACION_SPLASH = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
